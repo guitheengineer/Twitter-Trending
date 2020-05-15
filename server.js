396e5052355b6d404e3a0db4e8957e144405df23
guilhermesnd <guilhermesnd14@gmail.com>
2020-05-15 03:37:26 -0300

    changes to heroku deploy

--- server.js
+++ server.js
@@ -32,4 +32,14 @@ app.get("/api/trends/:place", (req, res) => {
   }
 });
 
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
 app.listen(port);
